f9a1ce12457dd9bc9410f8f233e9b12102d14a39
AndersonGamboaGamboa <andergg9@gmail.com>
2022-07-29 17:17:21 -0600

    Diceño de inicio de la aplicacion e inicios del login

---
+++ app/src/main/java/com/tiendavirtual/utils/MSPtextViewBold.kt
@@ -0,0 +1,17 @@
+package com.tiendavirtual.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class MSPtextViewBold(context: Context, attrs: AttributeSet) : AppCompatTextView (context, attrs){
+    init {
+        applyFront ()
+    }
+    private fun applyFront(){
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets,"Game Charger Regular.ttf")
+        setTypeface(typeface)
+    }
+}
