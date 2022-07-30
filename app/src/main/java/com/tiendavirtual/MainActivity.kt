e141445c8b76c21b3d184fa5e3b614f88b18f24f
AndersonGamboaGamboa <andergg9@gmail.com>
2022-07-30 13:30:07 -0600

    Login y register funcional

--- app/src/main/java/com/tiendavirtual/MainActivity.kt
+++ app/src/main/java/com/tiendavirtual/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.tiendavirtual
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.tiendavirtual.databinding.ActivityMainBinding
@@ -13,60 +15,76 @@ import com.tiendavirtual.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
-        setContentView(R.layout.activity_main)
+
+
 
         binding.btLogin.setOnClickListener {
             haceLogin();
         }
         binding.btRegister.setOnClickListener {
-                haceRegister();
+            haceRegister();
 
-            }
         }
+    }
 
-        private fun haceLogin() {
-            val email = binding.etEmail.text.toString()
-            val clave = binding.etClave.text.toString()
+    private fun haceLogin() {
+        val email = binding.etEmail.text.toString()
+        val clave = binding.etClave.text.toString()
 
-            auth.signInWithEmailAndPassword(email,clave)
-                .addOnCompleteListener(this){ task ->}
-            if(task,isSuccessful){
-                Log.d("Autenticando", "Logeado")
-                val user = auth.currentUser
-                actualiza(user)
-            }else{
-                Log.d("Creando usuario", "Fallo")
-                Toast.makeText(baseContext,"Fallo",Toast.LENGTH_LONG).show()
-                actualiza(null)
+        auth.signInWithEmailAndPassword(email, clave)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Autenticando", "Logeado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Log.d("Creando usuario", "La autentificacion fallo")
+                    Toast.makeText(baseContext, "Fallo", Toast.LENGTH_LONG).show()
+                    actualiza(null)
+                }
             }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
         }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val usuario = auth.currentUser
+        actualiza(usuario)
     }
 
     private fun haceRegister() {
         val email = binding.etEmail.text.toString()
         val clave = binding.etClave.text.toString()
 
-        auth.createUserWithEmailAndPassword(email,clave)
-            .addOnCompleteListener(this){ task ->}
-        if(task,isSuccessful){
-            Log.d("Creando usuario", "Registrado")
-            val user = auth.currentUser
-            actualiza(user)
-        }else{
-            Log.d("Creando usuario", "Fallo")
-            Toast.makeText(baseContext,"Fallo",Toast.LENGTH_LONG).show()
-            actualiza(null)
-        }
-        }
+        auth.createUserWithEmailAndPassword(email, clave)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Creando usuario", "Autenticando")
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Log.d("Autenticando", "La autentificacion fallo")
+                    Toast.makeText(baseContext, "Fallo", Toast.LENGTH_LONG).show()
+                    actualiza(null)
+                }
 
-fun actualiza(user: Any): Any {
+            }
+    }
+}
 
-}
-}
+
 
